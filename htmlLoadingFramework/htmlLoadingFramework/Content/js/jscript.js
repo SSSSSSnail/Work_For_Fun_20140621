2cf5b801a30a3de4c6df38c2af398e36474fc63c
Snail <warlocke@163.com>
2014-06-30 11:25:04 +0800

    step 2

--- htmlLoadingFramework/htmlLoadingFramework/Content/js/jscript.js
+++ htmlLoadingFramework/htmlLoadingFramework/Content/js/jscript.js
@@ -4,47 +4,93 @@ var answerNumberArray = [
     [2, 2, 2, 2, 2, 3],
     [2, 3, 4, 3, 3, 4]
 ];
+var doctorSaleMapping = [
+    [0, 1],
+    [0, 0, 1],
+    [1, 1, 1, 0],
+    [0, 0, 1],
+    [0, 1, 1]
+];
+var pathArray = ['111', '1101', '1100', '10', '011', '0101', '0100', '00-1-110', '00-1-111', '00-1-112', '00-1-10'];
+var answerSelectedArray;
+var doctorAnswerMapping;
 
+var path;
+
+var kmbgArray = [2, 2, 2, 2, 2, 2, 3, 2, 1, 1, 1];
+
+var keyMessage;
 
 $(document).on('pageinit', '#index',function () {
     $('#sales_button').click(function () {
         idtype = 0;
-        $.mobile.changePage('questionnaire.html', {transition: 'slide'});
+        answerSelectedArray = [-1, -1, -1, -1, -1, -1];
+        /*$.mobile.changePage('questionnaire.html', {transition: 'slide'});*/
+        $.mobile.changePage('keyMessage.html', {transition: 'slide'});
     });
     $('#doctor_button').click(function () {
         idtype = 1;
-        $.mobile.changePage('questionnaire.html', {transition: 'slide'});
+        answerSelectedArray = [-1, -1, -1, -1, -1, [false, false, false, false]];
+        doctorAnswerMapping = [-1, -1, -1, -1, -1, -1];
+
+        /*$.mobile.changePage('questionnaire.html', {transition: 'slide'});*/
+        $.mobile.changePage('keyMessage.html', {transition: 'slide'});
     });
+    currentQuestion = 0;
+    path = -1;
+    keyMessage = -1;
 });
 
 $(document).on('pageinit', '#questionnaire',function () {
     $('#back_to_index').click(function () {
-       alert('重新开始');
+        $.mobile.changePage('index.html', {transition: 'slide', reverse: true});
     });
 
     bindAction($('#main_content1'));
     bindAction($('#main_content2'));
 
-    $('#prevent').click(function () {
-        switchLeftOrRight('right');
-    });
     $('#next').click(function () {
         switchLeftOrRight('left');
     });
 
     switchLeftOrRight('init');
+
+    $('#photo').css({'background-image': 'url(images/p0.png)'});
 });
 
 function bindAction(selector) {
     selector.on('swipeleft',function () {
         switchLeftOrRight('left');
-    }).on('swiperight',function () {
-            switchLeftOrRight('right');
-        }).children('.question_answer_content').children().each(function (index) {
+    }).children('.question_answer_content').children().each(function (index) {
             $(this).click(function () {
-                selector.children('.question_answer_content').find('.question_answer_checkbox').css({'background-image': 'url(images/checkbox_uncheck.png)'});
-                $(this).children('.question_answer_checkbox').css({'background-image': 'url(images/checkbox_checked.png)'});
-                alert('选中'+index);
+                if (currentQuestion == 5 && idtype == 1) {
+                    var ifCheckboxSelected = answerSelectedArray[currentQuestion][index];
+                    answerSelectedArray[currentQuestion][index] = !ifCheckboxSelected;
+                    if (!ifCheckboxSelected) {
+                        $(this).children('.question_answer_checkbox').css({'background-image': 'url(images/checkbox_checked.png)'});
+                    } else {
+                        $(this).children('.question_answer_checkbox').css({'background-image': 'url(images/checkbox_uncheck.png)'});
+                    }
+                    var isSelectedAll = true;
+                    for (var i = 0; i < answerSelectedArray[currentQuestion].length; i++) {
+                        if (!answerSelectedArray[currentQuestion][i]) {
+                            isSelectedAll = false;
+                            break;
+                        }
+                    }
+                    if (isSelectedAll) {
+                        doctorAnswerMapping[currentQuestion] =  answerSelectedArray[currentQuestion - 1];
+                    } else {
+                        doctorAnswerMapping[currentQuestion] = 0;
+                    }
+                } else {
+                    selector.children('.question_answer_content').find('.question_answer_checkbox').css({'background-image': 'url(images/checkbox_uncheck.png)'});
+                    $(this).children('.question_answer_checkbox').css({'background-image': 'url(images/checkbox_checked.png)'});
+                    answerSelectedArray[currentQuestion] = index;
+                    if (idtype == 1) {
+                        doctorAnswerMapping[currentQuestion] = doctorSaleMapping[currentQuestion][index];
+                    }
+                }
             });
         });
 }
@@ -57,11 +103,54 @@ function switchLeftOrRight(direction) {
     if (direction == 'left') {
         fromDirection = 'left';
         toDirection = 'right';
-        if (currentQuestion == 5) {
-            alert('已经是最后一题了');
+
+        if (currentQuestion != 5 && answerSelectedArray[currentQuestion] == -1) {
+            alert('请先选择答案！');
             return;
         }
-        currentQuestion ++;
+        if (currentQuestion == 5) {
+            if (idtype == 0) {
+                if (answerSelectedArray[currentQuestion] == -1) {
+                    alert('请先选择答案！');
+                    return;
+                }
+            } else {
+                var hasSelected = false;
+                for (var i = 0; i < 4; i++) {
+                    if (answerSelectedArray[currentQuestion][i] == true) {
+                        hasSelected = true;
+                        break;
+                    }
+                }
+                if (!hasSelected) {
+                    alert('请先选择答案！');
+                    return;
+                }
+            }
+        }
+        //调整下一次数据
+        var answerString = '';
+
+        for (var j = 0; j < 6; j++) {
+            if (idtype == 0) {
+                answerString += answerSelectedArray[j];
+            } else {
+                answerString += doctorAnswerMapping[j];
+            }
+        }
+        for (var k = 0; k < pathArray.length; k++) {
+            if (answerString.indexOf(pathArray[k]) == 0) {
+                path = k;
+                $.mobile.changePage('showPath.html', {transition: 'slide'});
+                return;
+            }
+        }
+        if (answerString == '00-1-1-1-1') {
+            currentQuestion += 3;
+        } else {
+            currentQuestion ++;
+        }
+
     } else if (direction == 'right') {
         fromDirection = 'right';
         toDirection = 'left';
@@ -85,6 +174,20 @@ function switchLeftOrRight(direction) {
     toSelector.children('.question_answer_content').children().each(function (index) {
         $(this).removeClass('question_answer_bg');
         $(this).removeClass('question_answer_bg_last');
+        var checkboxSelector = $(this).children('.question_answer_checkbox');
+        if (currentQuestion == 5 && idtype == 1) {
+            if (answerSelectedArray[currentQuestion][index]) {
+                checkboxSelector.css({'background-image': 'url(images/checkbox_checked.png)'});
+            } else {
+                checkboxSelector.css({'background-image': 'url(images/checkbox_uncheck.png)'});
+            }
+        } else {
+            if (index == answerSelectedArray[currentQuestion]) {
+                checkboxSelector.css({'background-image': 'url(images/checkbox_checked.png)'});
+            } else {
+                checkboxSelector.css({'background-image': 'url(images/checkbox_uncheck.png)'});
+            }
+        }
         if (index < answerNumber) {
             var answerClass;
             if (index == answerNumber - 1) {
@@ -102,171 +205,39 @@ function switchLeftOrRight(direction) {
     if (direction != 'init') {
         fromSelector.hide( 'drop', { direction: fromDirection });
         toSelector.show( 'drop', { direction: toDirection });
+        if (idtype == 1) {
+            $('#photo').hide('fade', function () {
+                $(this).css({'background-image': 'url(images/p' + currentQuestion + '.png)'}).show('fade');
+            });
+        }
     }
 }
 
-/*var rightSection1CellSelectedIndex = -1;
-var rightSection2CellSelectedIndex = -1;
-var originalRightSection2CellSelectedIndex = -1;
-var currPageNumber = -1;
-var toPageNumber = -1;
-var pptIndexArray = [
-    [9, 15, 19, 27],
-    [6, 8, 10, 21],
-    [2, 6, 10, 21]
-];
-
-$(document).on("pageinit", "#index",function () {
-    $('.rightSectionCell').each(function (index) {
-        $(this).css({'background-image': 'url(images/rightSectionCell_' + index + '.png)'}).click(function () {
-            rightSection1CellSelectedIndex = index;
-            rightSection2CellSelectedIndex = -1;
-            originalRightSection2CellSelectedIndex = -1;
-            $.mobile.changePage("index2.html", {transition: "slide"});
-        });
-    });
-}).on('pageshow', "#index",function () {
-        $('#dancers').show('scale', 600);
-        $('#shadow').fadeIn(600);
-        $('#index_rightTitle').show('drop', {direction: 'up'}, 200, function () {
-            $('.rightSectionCell').eq(0).show('drop', {direction: 'up'}, 200, function () {
-                $('.rightSectionCell').eq(1).show('drop', {direction: 'up'}, 200, function () {
-                    $('.rightSectionCell').eq(2).show('drop', {direction: 'up'}, 200);
-                });
-            });
-        });
-    }).on('pagehide', "#index", function () {
-
-    });
-
-$(document).on("pageinit", "#index2",function () {
-    var index2_rightCell_Selector = $(this).find('.index2_rightSectionCell');
-    $('#index2_backButton').click(function () {
-        $.mobile.changePage("index.html", {transition: "slide", reverse: true});
-    });
-    $('#index2_rightTitle').css({'background-image': 'url(images/index2_rightTitle_' + rightSection1CellSelectedIndex + '.png)'});
-    if (rightSection2CellSelectedIndex != -1) {
-        index2_rightCell_Selector.eq(rightSection2CellSelectedIndex).children('.index2_rightSectionCell_checkBox').css({'background-image': 'url(images/index2_checkbox_checked.png)'});
+$(document).on('pageinit', '#showPath',function () {
+    var showPathBGSelector = $('#showPath_bg');
+    if (idtype == 0) {
+        showPathBGSelector.css({'background-image': 'url(images/showPath_sale_'+ path +'.png)'});
+    } else {
+        showPathBGSelector.css({'background-image': 'url(images/showPath_doctor.png)'});
     }
-    index2_rightCell_Selector.each(function (index) {
-        $(this).children('.index2_rightSectionCell_title').css({'background-image': 'url(images/index2_rightSectionCellTitle_' + rightSection1CellSelectedIndex + '_' + index + '.png)'});
-    }).click(function () {
-            if (rightSection2CellSelectedIndex != -1) {
-                index2_rightCell_Selector.eq(rightSection2CellSelectedIndex).children('.index2_rightSectionCell_checkBox').css({'background-image': 'url(images/index2_checkbox_unchecked.png)'});
-            }
-            $(this).children('.index2_rightSectionCell_checkBox').css({'background-image': 'url(images/index2_checkbox_checked.png)'});
-            rightSection2CellSelectedIndex = index2_rightCell_Selector.index(this);
-            $.mobile.changePage("index3.html", {transition: "slide", reloadPage: true});
-        });
-}).on('pageshow', "#index2", function () {
-        if (rightSection2CellSelectedIndex == -1) {
-            $('#index2_rightTitle').show('drop', {direction: 'up'}, 200, function () {
-                $('.index2_rightSectionCell').eq(0).show('drop', {direction: 'up'}, 200, function () {
-                    $('.index2_rightSectionCell').eq(1).show('drop', {direction: 'up'}, 200, function () {
-                        $('.index2_rightSectionCell').eq(2).show('drop', {direction: 'up'}, 200, function () {
-                            $('.index2_rightSectionCell').eq(3).show('drop', {direction: 'up'}, 200);
-                        });
-                    });
-                });
-            });
-        } else {
-            $('#index2_rightTitle').show();
-            $('.index2_rightSectionCell').show();
-        }
+    showPathBGSelector.on('swipeleft',function () {
+        $.mobile.changePage('keyMessage.html', {transition: 'slide'});
     });
+});
 
-$(document).on("pageinit", "#index3",function () {
-    var navigationBarSelector = $('#navigationBar');
-    navigationBarSelector.children('#barBackButton').click(function () {
-        $.mobile.changePage("index2.html", {transition: "slide", reverse: true});
-    });
-    navigationBarSelector.css({'background-image': 'url(images/batTitle_bg_' + rightSection1CellSelectedIndex + '.png)'}).children('.barTitle').each(function (index) {
-        $(this).click(function () {
-            navigationBarSelector.children('.barTitle').eq(rightSection2CellSelectedIndex).css({'background-image': 'url(images/barTitle_' + rightSection1CellSelectedIndex + '_' + rightSection2CellSelectedIndex + '_unselected.png)'});
-            $(this).css({'background-image': 'url(images/barTitle_' + rightSection1CellSelectedIndex + '_' + index + '_selected.png)'});
-            rightSection2CellSelectedIndex = index;
-            toPageNumber = (rightSection2CellSelectedIndex == 0 ? 1 : pptIndexArray[rightSection1CellSelectedIndex][rightSection2CellSelectedIndex - 1] + 1);
-            $('#mainContent').refreshContent();
-        });
-    });
-    navigationBarSelector.refreshBarTitles();
-    toPageNumber = (rightSection2CellSelectedIndex == 0 ? 1 : pptIndexArray[rightSection1CellSelectedIndex][rightSection2CellSelectedIndex - 1] + 1);
-    $('#mainContent').on('swipeleft',function () {
-        if (toPageNumber < pptIndexArray[rightSection1CellSelectedIndex][3]) {
-            toPageNumber++;
-        }
-        $(this).refreshContent();
-        for (var i = 0; i < 4; i++) {
-            if (toPageNumber <= pptIndexArray[rightSection1CellSelectedIndex][i]) {
-                rightSection2CellSelectedIndex = i;
-                break;
-            }
-        }
-        navigationBarSelector.refreshBarTitles();
-    }).on('swiperight',function () {
-            if (toPageNumber > 1) {
-                toPageNumber--;
-            }
-            $(this).refreshContent();
-            for (var i = 0; i < 4; i++) {
-                if (toPageNumber <= pptIndexArray[rightSection1CellSelectedIndex][i]) {
-                    rightSection2CellSelectedIndex = i;
-                    break;
+$(document).on('pageinit', '#keyMessage',function () {
+    path = 6;
+    $('#keyMessage_bg').css({'background-image': 'url(images/kmbg'+ kmbgArray[path] +'.png)'});
+
+    for (var i = 1; i < 4; i++) {
+        $('#message'+ i).addClass('message'+i+'_km'+ kmbgArray[path]).css({'background-image': 'url(images/message'+ i +'_' + path + '_unselected.png)'}).on('click', {forIndex: i}, function (param) {
+            $(this).css({'background-image': 'url(images/message'+ param.data.forIndex +'_' + path + '_selected.png)'});
+            for (var i = 1; i < 4; i++) {
+                if (i != param.data.forIndex) {
+                    $('#message'+ i).css({'background-image': 'url(images/message'+ i +'_' + path + '_unselected.png)'});
                 }
             }
-            navigationBarSelector.refreshBarTitles();
-        }).refreshContent();
-}).on('pageshow', "#index3", function () {
-
-    });
-
-$.fn.refreshBarTitles = function () {
-    if (originalRightSection2CellSelectedIndex == rightSection2CellSelectedIndex) return;
-    this.children('.barTitle').css({'background-image': 'url(images/barTitle_' + rightSection1CellSelectedIndex + '_' + index + '_unselected.png)'});
-    this.children('.barTitle').eq(rightSection2CellSelectedIndex).css({'background-image': 'url(images/barTitle_' + rightSection1CellSelectedIndex + '_' + index + '_selected.png)'});
-    this.children('.barTitle').each(function (index) {
-        if (index == rightSection2CellSelectedIndex) {
-            $(this).css({'background-image': 'url(images/barTitle_' + rightSection1CellSelectedIndex + '_' + index + '_selected.png)'});
-        } else {
-            $(this).css({'background-image': 'url(images/barTitle_' + rightSection1CellSelectedIndex + '_' + index + '_unselected.png)'});
-        }
-    });
-    originalRightSection2CellSelectedIndex = rightSection2CellSelectedIndex;
-};
-
-$.fn.refreshContent = function () {
-    if (this.is(":animated")) {
-        this.stop();
+            keyMessage = param.data.forIndex;
+        });
     }
-    var fromDiv = $('#mainContentAnimationFrom');
-    var toDiv = $('#mainContentAnimationTo');
-    this.css({'background-image': 'url(images/ppt' + rightSection1CellSelectedIndex + '/' + toPageNumber + '.png)'});
-    if (toPageNumber > currPageNumber) { //left
-        fromDiv.show().css({left: '1024px', 'background-image': 'url(images/ppt' + rightSection1CellSelectedIndex + '/' + toPageNumber + '.png)'}).animate({left: '0px'}, function () {
-            $(this).hide();
-        });
-        toDiv.show().css({left: '0px', 'background-image': 'url(images/ppt' + rightSection1CellSelectedIndex + '/' + currPageNumber + '.png)'}).animate({left: '-1024px'}, function () {
-            $(this).hide();
-        });
-    } else if (toPageNumber < currPageNumber) { //right
-        fromDiv.show().css({left: '-1024px', 'background-image': 'url(images/ppt' + rightSection1CellSelectedIndex + '/' + toPageNumber + '.png)'}).animate({left: '0px'}, function () {
-            $(this).hide();
-        });
-        toDiv.show().css({left: '0px', 'background-image': 'url(images/ppt' + rightSection1CellSelectedIndex + '/' + currPageNumber + '.png)'}).animate({left: '1024px'}, function () {
-            $(this).hide();
-        });
-    } else {
-        //nothing
-    }
-    currPageNumber = toPageNumber;
-};*/
-
-/*
- $(document).on("pageinit", "#keyPoint",function () {
-
- }).on('pageshow', "#keyPoint",function () {
-
- }).on('pagehide', "#keyPoint", function () {
-
- });
- */
+});
